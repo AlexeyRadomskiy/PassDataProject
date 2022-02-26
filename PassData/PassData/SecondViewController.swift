4319e1c731be7139252fddf3661ba415b6b9e70a
Alexey <alexeyradomskiy98@gmail.com>
2022-02-27 00:37:55 +0300

    Ver 1.0

---
+++ PassData/PassData/SecondViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.swift
+//  PassData
+//
+//  Created by Alexey on 26.02.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    var login: String?
+    
+    
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let login = self.login else { return }
+        label.text = "Hello, \(login)"
+    }
+    
+    @IBAction func goBackTapped(_ sender: UIButton) {
+        performSegue(withIdentifier: "unwindSegue", sender: nil)
+    }
+}
